4397aeacd35fbde6152ac349040d0d55007cd8c4
binogood <binogood68@gmai.com>
2021-02-19 16:38:46 +0900

    Add: product views.py 작성중

--- product/urls.py
+++ product/urls.py
@@ -5,4 +5,5 @@ from product.views import ProductView
 urlpatterns = [
         path('/product', ProductView.as_view()),
         path('/<int:category_id>', ProductView.as_view()),
+        path('/<int:category_id>/<str:sorting>', ProductView.as_view()),
 ]
