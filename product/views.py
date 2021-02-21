f87e509fd8ea4224ca44e2ff097815149495e064
binogood <binogood68@gmai.com>
2021-02-22 07:09:11 +0900

    Modify: product views.py 수정

--- product/views.py
+++ product/views.py
@@ -1,5 +1,6 @@
 import json
 from datetime            import datetime, timedelta
+from pytz                import utc
 
 from django.http         import JsonResponse
 from django.views        import View
@@ -17,31 +18,48 @@ from .models import (
         AnswerStatus,
 )
 
-def IsNew(products):
-    datetime_now = dt.datetime.today() - timedelta(days=30)
-    return [products for item in products if item.create_at < datetime_now]
+def isNew(create_at, compare_date):
+    return True if create_at > compare_date else False
 
-def IsBest(products):
-    return products.order_by('total_sales')
+def checkBestList():
+    best_query = Product.objects.all().order_by('-total_sales')[:10]
+    return [best.id for best in best_query]
+    product_list = Product.objects.all().order_by('total_sales')[:10] \
+        if category_id == 0 else Product.objects.filter(category_id=\
+        category_id).order_by(sorting)
 
-def IsSale(products):
-    return [products for item in products if item.sale > 0]
+def isBest(checkList, id):
+    return True if id in checkList else False 
 
-def IsLowPrice(products):
-    return products.order_by('-price')
-
-def IsHighPrice(products):
-    return products.order_by('price')
+def isSale(sale):
+    return True if sale > 0 else False
 
 
 class ProductListView(View):
     def get(self, request, **kwargs):
 
+        best_list = checkBestList()
         category_id = kwargs['category_id']
+        compare_date = utc.localize(datetime.utcnow()) - timedelta(days=30)
 
-        product_list = Product.objects.all().only('name').order_by('-total_sales') \
-        if category_id == 0 else Product.objects.filter(category_id=\
-        category_id).only('name').order_by('-total_sales')
+        if kwargs.get('sorting'):
+            sorting = kwargs['sorting']
+            if kwargs['sorting'] == 'isBest':
+                sorting = '\'-total_sales\''
+            elif kwargs['sorting'] == 'isNew':
+                sorting = '-create_at'
+            elif kwargs['sorting'] == 'isNew':
+                sorting = '\'-sale\''
+            elif kwargs['sorting'] == 'isLowPrice':
+                sorting = '\'price\''
+            elif kwargs['sorting'] == 'isHighPrice':
+                sorting = '\'-price\''
+        else :
+            sorting = '-total_sales'
+
+        product_list = Product.objects.all().order_by(sorting) \
+            if category_id == 0 else Product.objects.filter(category_id=\
+            category_id).order_by(sorting)
         
         products = [
                 {
@@ -51,30 +69,15 @@ class ProductListView(View):
                     'sale'          : item.sale,
                     'stock'         : item.stock,
                     'imageUrl'      : item.image_url,
-                    'category'      : item.category,
-                    'createAt'      : item.create_at,
-                    'totalSales'    : item.total_sales,
+                    'category'      : item.category.id,
+                    'isNew'         : isNew(item.create_at, compare_date),
+                    'isBest'        : isBest(best_list, item.id),
+                    'isSale'        : isSale(item.sale)
                 }for item in product_list] 
 
-#        print(products)
-#        if kwargs['sorting']:
-#            sorting = kwargs['sorting']
-#            if sorting == 'isNew':
-#                new = IsNew(products)
-#            elif sorting == 'isBest':
-#                best = IsBest(products) 
-#            elif sorting == 'isSale':
-#                best = IsSale(products) 
-#            elif sorting == 'isLowPrice':
-#                lowprice = IsLowPrice(products)
-#            elif sorting == 'isHighPrice':
-#                highprice = IsHighPrice(products)
-#            else :
-#                return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
-#
-        return JsonResponse({'message' : 'SUCCESS',
-                             'data' : product_list},
-                             status=200)
+        print(products)
+
+        return JsonResponse({'message' : 'SUCCESS'}, status=200)
 
 
 class ProductDetailView(View):
@@ -102,7 +105,6 @@ class ProductDetailView(View):
                     'id'             : image.id,
                     'imageUrl'       : image.image_url,
             } for image in images]
-
         product_reviews = [
             {
                     'id'             : review.id,
@@ -131,22 +133,6 @@ class ProductDetailView(View):
                                  'Images'   : product_images,
                                  'review'   : product_reviews,
 #                                 'inquirts' : product_inquirys
-                            }})
+                            }}, status=200)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
