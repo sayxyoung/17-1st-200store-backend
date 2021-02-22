be15b6d727fccf0a9856bc24460ee40ab9ddf826
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-02-22 19:11:50 +0900

    Modify: ProductLikeView 코드리뷰 수정 완료

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,6 @@ from django.urls   import path
 from .views import ProductLikeView
 
 urlpatterns = [
-    path('/productlike', ProductLikeView.as_view()),
-    path('/productlike/<int:user_id>', ProductLikeView.as_view())
+    path('/product_like', ProductLikeView.as_view()),
+    path('/product_like/<int:user_id>', ProductLikeView.as_view())
 ]
