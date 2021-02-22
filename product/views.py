41a934376c65032d6f1ac68d925df427bd2254dc
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-02-22 20:11:54 +0900

    Add: OrderListView Get, Post Method 추가

--- product/views.py
+++ product/views.py
@@ -3,7 +3,7 @@ import json
 from django.http  import JsonResponse
 from django.views import Views
 
-from 
+from order.models import Order, Cart
 
 class OrderListView(View):
     def get(self, request):
@@ -11,3 +11,12 @@ class OrderListView(View):
     
     def post(self, request):
         data = json.loads(request.body)
+
+        order_list = []
+        orders = Order.objects.filter(user_id=data['userId']) # decorator 되면 거기서 받아오기
+        
+        for order in orders:
+            products = Cart.objects.filter(order_id=order.id)
+            products = [{
+                
+            } for product in products]
