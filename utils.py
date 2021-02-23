327d89a72272f1d816dd9caa18237a5de9050490
sayxyoung <hyjlove11@gmail.com>
2021-02-23 10:44:15 +0900

    Add: 로그인 데코레이터 기능 추가

---
+++ utils.py
@@ -0,0 +1,26 @@
+import jwt
+
+from django.http import JsonResponse
+
+from my_settings import ALGORITHM
+from my_settings import SECRET_KEY
+from user.models import User
+
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers['Authorization']
+            payload      = jwt.decode(access_token, SECRET_KEY, algorithms=ALGORITHM)
+            user         = User.objects.get(pk=payload['user_pk'])
+            request.user = user
+
+            return func(self, request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message': 'INVALID_TOKEN'}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_USER'}, status=400)
+
+    return wrapper
