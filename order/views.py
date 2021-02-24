70197fd4cf5b66359ebd0e6dc3047a8422d7b7f1
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-02-24 16:44:45 +0900

    Add: OrderListView PATCH 추가

--- order/views.py
+++ order/views.py
@@ -12,8 +12,6 @@ from utils          import login_decorator
 class OrderListView(View):
     @login_decorator
     def get(self, request):
-        data   = json.loads(request.body)
-        
         compare_date = timezone.localtime() - timedelta(days=7)
         start_date   = request.GET.get('startDate', compare_date)
         end_date     = request.GET.get('endDate', timezone.localtime())
@@ -22,7 +20,7 @@ class OrderListView(View):
         
         result = [{
             'serialNumber' : order.serial_number,
-            'orderStatus'  : order.status,
+            'orderStatus'  : order.status.id,
             'orderDate'    : order.create_at,
             'orderId'      : order.id,
             'subProducts'  : [{
@@ -30,9 +28,30 @@ class OrderListView(View):
                 'name'          : cart.product.name,
                 'totalPrice'    : cart.total_price,
                 'quantity'      : cart.quantity,
-                'productStatus' : cart.status,
-                'isReview'      : True if is_review.exists() else False
+                'productStatus' : cart.status.id,
+                'isReview'      : MatchingReview.objects.filter(order=order.id, product=cart.product).exists()
             } for cart in Cart.objects.filter(order_id=order.id)]
         } for order in orders]
 
         return JsonResponse({'message':'SUCCESS', 'data': result}, status=200)
+
+    @login_decorator
+    def patch(self, request):
+        data = json.loads(request.body)
+
+        try:
+            # status_type = reqeust.GET.get('statusType', None)
+            status_id  = request.GET.get('status_id', None)
+            order_id   = data['orderId']
+            product_id = data['productId'] 
+
+            product           = Cart.objects.get(order_id=order_id, product_id=product_id)
+            product.status_id = status_id
+            product.save()
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+        except Cart.DoesNotExist:
+            return JsonResponse({'message':'DOES_NOT_EXIST'}, status=400)
+        
+        return JsonResponse({'message':'SUCCESS'}, status=200)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import jwt
+from json     import JSONDecodeError
 
 from django.http import JsonResponse
 
@@ -20,6 +21,9 @@ def login_decorator(func):
             return JsonResponse({'message': 'INVALID_TOKEN'}, status=400)
 
         except User.DoesNotExist:
+            return JsonResponse({'message': 'INVALID_USER'}, status=400)
+
+        except JSONDecodeError:
             return JsonResponse({'message': 'INVALID_USER'}, status=400)
 
     return wrapper
